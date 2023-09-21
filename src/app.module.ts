f3379e913946a03cf6f6bb559be9f804c48a7a8c
Oscar Trujillo <otrujillomunera@gmail.com>
2023-09-21 11:02:48 -0500

    Connect RabbitMQ
    Define connection to RabbitMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './users/controllers/app.controller';
+import { AppService } from './users/services/app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+    {
+      name: 'ORDERS_SERVICE',
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'orders_queue',
+        queueOptions: {
+          durable: false
+        },
+      },
+    },
+  ]),],
   controllers: [AppController],
   providers: [AppService],
 })
